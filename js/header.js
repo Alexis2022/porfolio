226cf03e12919ae09d2a375262ecf353b38702da
Alexis2022 <ervinalexisespanaanez@gmail.com>
2022-10-29 21:50:05 -0400

    agregando forEach al header

--- js/header.js
+++ js/header.js
@@ -4,8 +4,6 @@ const navLi = document.querySelectorAll(".ul-li");
 
 menu.addEventListener("click", ()=> nav.classList.toggle("menuNav"));
 
-navLi[0].addEventListener("click", ()=> nav.classList.toggle("menuNav"));
-navLi[1].addEventListener("click", ()=> nav.classList.toggle("menuNav"));
-navLi[2].addEventListener("click", ()=> nav.classList.toggle("menuNav"));
-navLi[3].addEventListener("click", ()=> nav.classList.toggle("menuNav"));
-navLi[4].addEventListener("click", ()=> nav.classList.toggle("menuNav"));
+navLi.forEach(element =>{
+    element.addEventListener("click", ()=> nav.classList.toggle("menuNav"));
+});
